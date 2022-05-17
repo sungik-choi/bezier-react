ae52b0d1b452e4e284210419f0f6f2d0a95f50d4
Ed Sungik Choi <sungik.dev@gmail.com>
2022-05-17 21:02:51 +0900

    refactor(worklets): worklet 패키지 분리 (기본 세팅)

---
+++ packages/bezier-react-worklets/src/EnableCSSHoudini.ts
@@ -0,0 +1,26 @@
+/* Internal dependencies */
+import SmoothCornersScript from './smoothCorners'
+
+interface EnableCSSHoudiniOptions {
+  smoothCorners: boolean
+}
+
+export const enableSmoothCorners = Object.seal({
+  current: false,
+})
+
+export function EnableCSSHoudini({
+  smoothCorners = false,
+}: EnableCSSHoudiniOptions) {
+  // NOTE: CSS namespace 에 접근 가능할 경우에만 사용
+  if (typeof CSS === 'undefined') { return }
+
+  if ('paintWorklet' in CSS) {
+    if (smoothCorners && !enableSmoothCorners.current) {
+      const workletURL = URL.createObjectURL(new Blob([SmoothCornersScript], { type: 'application/javascript' }))
+      // @ts-ignore
+      CSS.paintWorklet.addModule(workletURL)
+      enableSmoothCorners.current = true
+    }
+  }
+}
