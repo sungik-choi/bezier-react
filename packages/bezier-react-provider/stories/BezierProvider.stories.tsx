ee172962154c5d7e0824b57748ec776735516037
Ed Sungik Choi <sungik.dev@gmail.com>
2022-05-17 21:48:07 +0900

    refactor(provider): 스토리의 잘못된 import 수정 및 주석 추가

--- packages/bezier-react-provider/stories/BezierProvider.stories.tsx
+++ packages/bezier-react-provider/stories/BezierProvider.stories.tsx
@@ -1,14 +1,15 @@
 /* External dependencies */
 import React from 'react'
 import base from 'paths.macro'
-import { Story, Meta } from '@storybook/react'
+import type { Story, Meta } from '@storybook/react'
+import { DarkFoundation, LightFoundation } from '@channel.io/bezier-react-foundation'
+import { getTitle } from '@channel.io/bezier-react-utils'
 
 /* Internal dependencies */
-import { styled, DarkFoundation, LightFoundation } from 'Foundation'
-import { getTitle } from 'Utils/storyUtils'
-import { ButtonSize, ButtonStyleVariant, ButtonColorVariant } from 'Components/Button/Button.types'
-import { Button } from 'Components/Button'
-import BezierProvider from '../src/BezierProvider'
+import { BezierProvider } from '../src/BezierProvider'
+// FIXME-Monorepo(@ed): 의존성 제거
+// import { ButtonSize, ButtonStyleVariant, ButtonColorVariant } from 'Components/Button/Button.types'
+// import { Button } from 'Components/Button'
 
 interface BezierProviderStorybookProps {
   foundation: 'dark' | 'light'
@@ -30,19 +31,19 @@ export default {
   },
 } as Meta
 
-const ButtonWrapper = styled(Button)``
+// const ButtonWrapper = styled(Button)``
 
 const Template: Story<BezierProviderStorybookProps> = ({ foundation }) => (
   <BezierProvider
     foundation={foundation === 'dark' ? DarkFoundation : LightFoundation}
-    themeVarsScope={ButtonWrapper}
+    // themeVarsScope={ButtonWrapper}
   >
-    <ButtonWrapper
+    { /* <ButtonWrapper
       text="Try to switch foundation"
       size={ButtonSize.M}
       styleVariant={ButtonStyleVariant.Primary}
       colorVariant={ButtonColorVariant.Blue}
-    />
+    /> */ }
   </BezierProvider>
 )
 
